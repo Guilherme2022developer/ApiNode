549a0bab58129c04072900cac26741938f6f4370
Guilherme <guilhermealves4548@gmail.com>
2022-05-11 22:43:13 -0300

    banco de dados mongo

--- app/models/note.js
+++ app/models/note.js
@@ -12,6 +12,5 @@ let noteSchema = new mongoose.Schema({
   }
 })
 
-noteSchema.index({'title': 'text', 'body': 'text'});
 
 module.exports = mongoose.model('Note', noteSchema);
--- config/database.js
+++ config/database.js
@@ -4,6 +4,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/javascriptNote', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
 }).then(() => console.log('Connection succesful'))
     .catch((err) => console.log(err));
